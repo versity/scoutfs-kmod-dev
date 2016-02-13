ae6a605e03bc9d3d8f5acf7e3f6ec65d81f5f36b
Zach Brown <zab@versity.com>
2016-02-12 19:28:03 -0800

    Add simple message printing
    Add a message printing function whose output includes the device and
    major:minor and which handles the kernel level string prefix.
    
    Signed-off-by: Zach Brown <zab@versity.com>

---
+++ src/msg.c
@@ -0,0 +1,20 @@
+#include <linux/kernel.h>
+#include <linux/fs.h>
+
+void scoutfs_msg(struct super_block *sb, const char *prefix, const char *str,
+		 const char *fmt, ...)
+{
+        struct va_format vaf;
+        va_list args;
+
+        va_start(args, fmt);
+
+        vaf.fmt = fmt;
+        vaf.va = &args;
+
+        printk("%sscoutfs (%s %u:%u)%s: %pV\n", prefix,
+	       sb->s_id, MAJOR(sb->s_bdev->bd_dev), MINOR(sb->s_bdev->bd_dev),
+	       str, &vaf);
+
+        va_end(args);
+}
---
+++ src/msg.h
@@ -0,0 +1,16 @@
+#ifndef _SCOUTFS_MSG_H_
+#define _SCOUTFS_MSG_H_
+
+void scoutfs_msg(struct super_block *sb, const char *prefix, const char *str,
+		 const char *fmt, ...);
+
+#define scoutfs_err(sb, fmt, args...) \
+	scoutfs_msg(sb, KERN_ERR, " error", fmt, ##args)
+
+#define scoutfs_warn(sb, fmt, args...) \
+	scoutfs_msg(sb, KERN_WARNING, " warning", fmt, ##args)
+
+#define scoutfs_info(sb, fmt, args...) \
+	scoutfs_msg(sb, KERN_INFO, "", fmt, ##args)
+
+#endif

c847e085795acae8d4d92e34548b5f3dd7a7f519
Zach Brown <zab@versity.com>
2016-11-17 15:55:25 -0800

    Add printf format checking to scoutfs msg funcs
    scoutfs_msg() was missing the attribute to check printf formats and
    arguments.
    
    Signed-off-by: Zach Brown <zab@versity.com>
    Reviewed-by: Mark Fasheh <mfasheh@versity.com>

--- src/msg.h
+++ src/msg.h
@@ -1,8 +1,8 @@
 #ifndef _SCOUTFS_MSG_H_
 #define _SCOUTFS_MSG_H_
 
-void scoutfs_msg(struct super_block *sb, const char *prefix, const char *str,
-		 const char *fmt, ...);
+void __printf(4, 5) scoutfs_msg(struct super_block *sb, const char *prefix,
+				const char *str, const char *fmt, ...);
 
 #define scoutfs_err(sb, fmt, args...) \
 	scoutfs_msg(sb, KERN_ERR, " error", fmt, ##args)

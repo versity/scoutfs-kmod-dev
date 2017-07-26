0baef6e348daef36e6a179ba96d72b2ecfffd1fb
Zach Brown <zab@versity.com>
2017-07-26 13:56:20 -0700

    scoutfs: add endian_swap.h
    Add a helper header for conversions between little and big endian.
    
    Signed-off-by: Zach Brown <zab@versity.com>

---
+++ src/endian_swap.h
@@ -0,0 +1,10 @@
+#ifndef _SCOUTFS_ENDIAN_SWAP_H_
+#define _SCOUTFS_ENDIAN_SWAP_H_
+
+#define le32_to_be32(x) cpu_to_be32(le32_to_cpu(x))
+#define le16_to_be16(x) cpu_to_be16(le16_to_cpu(x))
+
+#define be32_to_le32(x) cpu_to_le32(be32_to_cpu(x))
+#define be16_to_le16(x) cpu_to_le16(be16_to_cpu(x))
+
+#endif

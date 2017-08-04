f367c138681287312bc106ae39e942d008a312ed
Mark Fasheh <mfasheh@versity.com>
2017-08-04 16:42:46 -0500

    scoutfs: provide ->llseek
    Without this we return -ESPIPE when a process tries to seek on a regular
    file.
    
    Signed-off-by: Mark Fasheh <mfasheh@versity.com>

--- src/data.c
+++ src/data.c
@@ -1258,6 +1258,7 @@ const struct file_operations scoutfs_file_fops = {
 	.aio_write	= generic_file_aio_write,
 	.unlocked_ioctl	= scoutfs_ioctl,
 	.fsync		= scoutfs_file_fsync,
+	.llseek		= generic_file_llseek,
 };
 
 

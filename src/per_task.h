026e5559483e52d8eee1852a777b6307ad3f8c5f
Zach Brown <zab@versity.com>
2019-04-19 11:00:02 -0700

    scoutfs: expand the per_task API
    This adds some minor functionality to the per_task API for use by the
    upcoming offline waiting work.
    
    Add scoutfs_per_task_add_excl() so that a caller can tell if their task
    was already put on a per-task list by their caller.
    
    Make scoutfs_per_task_del() return a bool to indicate if the entry was
    found on a list and was in fact deleted, or not.
    
    Add scoutfs_per_task_init_entry() for initializing entries that aren't
    declared on the stack.
    
    Signed-off-by: Zach Brown <zab@versity.com>

--- src/per_task.h
+++ src/per_task.h
@@ -21,8 +21,11 @@ struct scoutfs_per_task_entry {
 void *scoutfs_per_task_get(struct scoutfs_per_task *pt);
 void scoutfs_per_task_add(struct scoutfs_per_task *pt,
 			  struct scoutfs_per_task_entry *ent, void *ptr);
-void scoutfs_per_task_del(struct scoutfs_per_task *pt,
+bool scoutfs_per_task_add_excl(struct scoutfs_per_task *pt,
+			       struct scoutfs_per_task_entry *ent, void *ptr);
+bool scoutfs_per_task_del(struct scoutfs_per_task *pt,
 			  struct scoutfs_per_task_entry *ent);
 void scoutfs_per_task_init(struct scoutfs_per_task *pt);
+void scoutfs_per_task_init_entry(struct scoutfs_per_task_entry *ent);
 
 #endif

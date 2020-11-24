897f172d4f4f329c96ab311b1be0dca23f2732a6
Zach Brown <zab@versity.com>
2020-11-24 12:15:13 -0800

    scoutfs: convert endian in SCOUTFS_IS_META_BDEV
    We missed that flags is le64.
    
    Signed-off-by: Zach Brown <zab@versity.com>

--- src/super.h
+++ src/super.h
@@ -98,7 +98,7 @@ static inline bool SCOUTFS_HAS_SBI(struct super_block *sb)
 
 static inline bool SCOUTFS_IS_META_BDEV(struct scoutfs_super_block *super_block)
 {
-	return !!(super_block->flags & SCOUTFS_FLAG_IS_META_BDEV);
+	return !!(le64_to_cpu(super_block->flags) & SCOUTFS_FLAG_IS_META_BDEV);
 }
 
 #define SCOUTFS_META_BDEV_MODE (FMODE_READ | FMODE_WRITE | FMODE_EXCL)
